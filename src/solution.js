16383527f4455ee30fbcbcaeaa6239214072e952
cortezrc6 <82867552+cortezrc6@users.noreply.github.com>
2023-09-08 00:55:44 +0800

    Check-for-even-or-odd-Rolly-Cortez-CpE-401

--- src/solution.js
+++ src/solution.js
@@ -1,4 +1,26 @@
-// Function to check if a number is even or odd
-function checkEvenOrOdd(_) {
-  // Put your code here
-}
+document.addEventListener('DOMContentLoaded', function () {
+  const numberForm = document.getElementById('numberForm');
+  const numberInput = document.getElementById('numberInput');
+  const resultElement = document.getElementById('result');
+
+  numberForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const userInput = numberInput.value;
+    const result = checkEvenOrOdd(userInput);
+    resultElement.textContent = result;
+  });
+
+  function checkEvenOrOdd(num) {
+    const parsedNum = parseInt(num);
+
+    const isInteger = Number.isInteger(parsedNum);
+    const sign = parsedNum < 0 ? 'Negative' : 'Positive';
+    const isEven = parsedNum % 2 === 0;
+
+    const result = isInteger
+      ? `${sign} ${isEven ? 'Even' : 'Odd'} Number`
+      : 'Error: Input is not an integer';
+
+    return result;
+  }
+});
