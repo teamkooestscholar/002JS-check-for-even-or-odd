29479569d1f55c8c2f4f8b6015974b988b3d50db
gemoftruesight <kcdsanchezz@gmail.com>
2023-09-08 16:36:35 +0800

    update

--- src/solution.js
+++ src/solution.js
@@ -1,4 +1,22 @@
-// Function to check if a number is even or odd
-function checkEvenOrOdd(_) {
-  // Put your code here
+function EvorOdd(num){
+  const result  = (num % 2 ==0)? "Even" : "Odd";
+  return result;               
+};
+
+function PstvrNgtv(num){
+  const sgn = (num >0)? "Postive" :"Negative";
+  return sgn;
+}
+
+while(true){
+  const numm = parseFloat(prompt("Enter a number: "));
+
+  if(!isNaN(numm)){
+      const rslt = EvorOdd(numm);
+      const sgnn = PstvrNgtv(numm);
+      alert("The number is " + (sgnn)+" "+(rslt));
+  }
+  else{
+      alert("Enter Integer only");
+  }
 }
