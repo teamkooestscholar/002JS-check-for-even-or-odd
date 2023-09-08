07563ba30f42cfbd610b32f3811a941d4cec0a04
lindraaa <chancruz739@gmail.com>
2023-09-08 14:49:27 +0800

    function added

--- src/solution.js
+++ src/solution.js
@@ -1,4 +1,27 @@
-// Function to check if a number is even or odd
-function checkEvenOrOdd(_) {
-  // Put your code here
+function checkEvenOrOdd(num){
+  const result  = (num % 2 ==0)? "Even" : "Odd";
+  return result;
+  
+                   
+};
+function PositiveorNegative(num){
+  const sign = (num >0)? "Postive" :"Negative";
+  return sign;
+}
+
+
+while(true){
+  const num = parseFloat(prompt("Enter a number: "));
+
+  if(!isNaN(num)){
+      const res = checkEvenOrOdd(num);
+      const sig = PositiveorNegative(num);
+      alert("The number is " + (sig)+" "+(res));
+  }
+  else{
+      alert("Enter Integer only")
+  }
+
+  
+
 }
