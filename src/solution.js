23eb233b0aaa7685ce05c0767b968cbbe841048b
deguzmankaira <123879555+deguzmankaira@users.noreply.github.com>
2023-09-09 23:45:35 +0800

    Solution to EASY: 002 JS

--- src/solution.js
+++ src/solution.js
@@ -1,4 +1,22 @@
 // Function to check if a number is even or odd
-function checkEvenOrOdd(_) {
-  // Put your code here
+function checkEvenOrOdd(input) {
+  const result = new Map([
+    [true, new Map([[true, "Even and Positive"], [false, "Even and Negative"]])],
+    [false, new Map([[true, "Odd and Positive"], [false, "Odd and Negative"]])]
+  ]);
+
+  const isInteger = !isNaN(input) && Number.isInteger(input);
+
+  return isInteger
+    ? result.get(input % 2 === 0).get(input > 0) || "Even"
+    : "Try again. Enter an integer.";
 }
+
+let input;
+do {
+  input = prompt("Enter a number to check if even or odd:");
+  input = parseInt(input);
+
+  const result = checkEvenOrOdd(input);
+  alert(result);
+} while (!Number.isInteger(input));
