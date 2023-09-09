72afb0df44c1dd45de5e540c3ae1ef14cb0bab1c
Cy12swe <104562626+Cy12swe@users.noreply.github.com>
2023-09-10 01:26:23 +0800

    Checks if the number is even or odd, positve or negative. Also checks if the number is an integer

--- src/solution.js
+++ src/solution.js
@@ -1,4 +1,24 @@
-// Function to check if a number is even or odd
-function checkEvenOrOdd(_) {
-  // Put your code here
+function checkEvenOrOdd(number) {
+  if (typeof number !== 'number' || !Number.isInteger(number)) {
+    return 'Error: Input is not an integer';
+  }
+
+  if (number === 0) {
+    return 'Zero is neither even nor odd';
+  } else if (number % 2 === 0) {
+    if (number > 0) {
+      return 'Positive Even';
+    } else {
+      return 'Negative Even';
+    }
+  } else {
+    if (number > 0) {
+      return 'Positive Odd';
+    } else {
+      return 'Negative Odd';
+    }
+  }
 }
+
+
+console.log(checkEvenOrOdd(-10));
