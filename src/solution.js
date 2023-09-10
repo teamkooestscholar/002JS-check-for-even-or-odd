16123dbbc235789bc5fc1f43e903d47be7537021
ysabelandreii <123624285+ysabelandreii@users.noreply.github.com>
2023-09-10 14:59:04 +0800

    Update solution.js
    Submission for solution.js

--- src/solution.js
+++ src/solution.js
@@ -1,4 +1,27 @@
-// Function to check if a number is even or odd
-function checkEvenOrOdd(_) {
-  // Put your code here
-}
+document.addEventListener('DOMContentLoaded', function () {
+    const checkButton = document.getElementById('checkButton');
+    const resultElement = document.getElementById('result');
+  
+    checkButton.addEventListener('click', function () {
+      const userInput = document.getElementById('userInput').value;
+      const number = parseFloat(userInput);
+  
+      function checkEvenOrOdd(number) {
+        const isInteger = typeof number === 'number' && Number.isInteger(number);
+        const parity = ["Even", "Odd"];
+        const signs = ["Positive", "Negative"];
+        const isEven = Math.abs(Math.floor(number) % 2) === 0;
+        const sign = Math.sign(number);
+  
+        const isNotInteger = !isInteger ? "ERROR: Input is not an integer" : "";
+        const parityIndex = isEven ? 0 : 1;
+        const signIndex = sign === 1 ? 0 : 1;
+  
+        return isNotInteger || `${signs[signIndex]} ${parity[parityIndex]}`;
+      }
+  
+      const result = isNaN(number) ? "Please enter a valid number." : checkEvenOrOdd(number);
+      resultElement.textContent = result;
+    });
+  });
+  
