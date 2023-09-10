838b125e2d5b96ed5ebace46b6e76cd1c033b07a
shinteo <124492024+ShinPio@users.noreply.github.com>
2023-09-10 15:26:28 +0800

    Check For Even or Odd

--- src/solution.js
+++ src/solution.js
@@ -1,4 +1,25 @@
-// Function to check if a number is even or odd
-function checkEvenOrOdd(_) {
-  // Put your code here
+ // Bonus Challenge 1
+ const isInteger = typeof number === 'number' && Number.isInteger(number);
+
+ // Bonus Challenge 2
+ const results = ["Even", "Odd"];
+ const index = (Math.abs(Math.floor(number)) % 2);
+ const result = results[index];
+
+ if (!isInteger) {
+   return "Error:The number is not and integer";
+ }
+
+ // Bonus Challenge 3
+ if (number === 0) {
+   return "Zero";
+ }
+
+ return (result === "Even" ? "Even" : "Odd") + (Math.sign(number) === 1 ? " Positive" : " Negative");
 }
+
+// Test cases
+console.log(checkEvenOrOdd(-24));       // "Positive Even"
+console.log(checkEvenOrOdd(7));      // "Negative Odd"
+console.log(checkEvenOrOdd(0));       // "Zero"
+console.log(checkEvenOrOdd(2.7));     // "Error: Input is not an integer"
