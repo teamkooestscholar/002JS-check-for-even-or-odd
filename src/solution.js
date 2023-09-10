a6c070c284db98dc8a66fa4cc330fae6db3119aa
DarrellBagsic <123875127+DarrellBagsic@users.noreply.github.com>
2023-09-10 20:48:41 +0800

    integer

--- src/solution.js
+++ src/solution.js
@@ -1,4 +1,27 @@
-// Function to check if a number is even or odd
-function checkEvenOrOdd(_) {
-  // Put your code here
+function checkNumberType() {
+  const userInput = prompt("Please enter an integer:");
+  const num = parseFloat(userInput);
+
+  const isInteger = typeof num === 'number' && isFinite(num) && Math.floor(num) === num;
+
+  if (!isInteger) {
+    alert("Error! not an integer");
+    return;
+  }
+
+  const isEven = Math.abs(num % 2) === 0;
+  const isPositive = Math.sign(num) === 1;
+
+  if (isEven && isPositive) {
+    alert("Positive Even");
+  } else if (isEven && !isPositive) {
+    alert("Negative Even");
+  } else if (!isEven && isPositive) {
+    alert("Positive Odd");
+  } else {
+    alert("Negative Odd");
+  }
 }
+
+// Call the function to get user input and display the result
+checkNumberType();
