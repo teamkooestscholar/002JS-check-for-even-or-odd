4169b42b22a6af8da65d034f733dfe25663dc9b9
jlfx09 <142736901+jlfx09@users.noreply.github.com>
2023-09-10 12:01:33 +0800

    added solution.js and updated index.html

--- src/solution.js
+++ src/solution.js
@@ -1,4 +1,24 @@
-// Function to check if a number is even or odd
-function checkEvenOrOdd(_) {
-  // Put your code here
+function checkEvenOrOdd(input) {
+  // Check if the input is a number
+  const errorMessage = (typeof input !== 'number' || isNaN(input)) ? 'Error: Input is not a valid number' : '';
+
+  
+  const integerInput = Math.floor(input); //makes sure that the input of the user is an integer
+
+ 
+  const sign = Math.sign(integerInput);
+  const signMessage = (sign === 1) ? 'Positive' : (sign === -1) ? 'Negative' : 'Zero';  // Determine whether the number is positive, negative, or zero
+
+  const isEven = (Math.abs(integerInput) & 1) === 0;
+  const parityMessage = isEven ? 'Even' : 'Odd';
+
+  const result = errorMessage || `${integerInput} is ${signMessage} ${parityMessage}`;
+  return result;
 }
+
+// Prompt the user for input
+const userInputString = prompt('Enter an integer:');
+const userInput = parseFloat(userInputString);
+
+const result = checkEvenOrOdd(userInput);
+console.log(result || 'Error: Invalid input. Please enter a number.');
