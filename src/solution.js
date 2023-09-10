ad29ebd84bb94123b6ed9a0cd4832d1e702c4506
PlopN <70200493+TadWeird@users.noreply.github.com>
2023-09-10 17:30:27 +0800

    Addition of Css and Html and such

--- src/solution.js
+++ src/solution.js
@@ -1,4 +1,29 @@
-// Function to check if a number is even or odd
-function checkEvenOrOdd(_) {
-  // Put your code here
+// Function to check if a number is even
+function checkEvenOrOdd(num)
+{
+    return num % 2 === 0;
 }
+  
+  // Function to determine if the input is even or odd or return an error message without using "if"
+  function TestTheOddsEven(input) {
+    // Use the ternary operator to check if input is not a number
+    const result =
+      typeof input === 'number' && !isNaN(input) && Number.isInteger(input)
+        ? checkEvenOrOdd(input)
+          ? input >= 0
+            ? 'Positive Even'
+            : 'Negative Even'
+          : input >= 0
+            ? 'Positive Odd'
+            : 'Negative Odd'
+        : 'Error: Please enter a valid integer';
+  
+    return result;
+  }
+  
+    document.getElementById('evenOddForm').addEventListener('submit', function (e) 
+    {
+    e.preventDefault();
+    const userInput = parseInt(document.getElementById('numberInput').value);
+    const message = TestTheOddsEven(userInput);
+    document.getElementById('result').textContent = message;});
