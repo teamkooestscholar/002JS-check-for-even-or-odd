55f74df8ece9f4fef967abaf5759b1981e36066d
ChristianPaulo24 <123344269+ChristianPaulo24@users.noreply.github.com>
2023-09-10 22:37:40 +0800

    solution.js

--- src/solution.js
+++ src/solution.js
@@ -1,4 +1,23 @@
 // Function to check if a number is even or odd
-function checkEvenOrOdd(_) {
-  // Put your code here
-}
+function checkEvenOrOdd(number) {
+    if (number % 2 === 0) {
+      return 'Even';
+    } else {
+      return 'Odd';
+    }
+  }
+  
+  // Prompt the user for input
+  const userInput = prompt('Enter a number:');
+  
+  // Convert the user input to a number
+  const number = parseInt(userInput);
+  
+  // Check if the input is a valid number
+  if (!isNaN(number)) {
+    const result = checkEvenOrOdd(number);
+    console.log(`${number} is ${result}.`);
+  } else {
+    console.log('Please enter a valid number.');
+  }
+  
