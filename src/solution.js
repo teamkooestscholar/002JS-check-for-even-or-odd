147720cb635cb907f299860f22af50fe6e997526
ChristianJoyGaray <123853568+ChristianJoyGaray@users.noreply.github.com>
2023-09-10 19:00:45 +0800

    new

--- src/solution.js
+++ src/solution.js
@@ -1,4 +1,22 @@
 // Function to check if a number is even or odd
-function checkEvenOrOdd(_) {
-  // Put your code here
+function checkEvenOrOdd(number) {
+   
+  const results = [
+    ["Positive Even", "Positive Odd"],
+    ["Negative Even", "Negative Odd"]
+  ];
+
+  const isInteger = Number.isInteger(number);
+  const remainder = Math.abs(Math.floor(number) % 2);
+  const sign = Math.sign(number);
+
+  const output = isInteger ? results[sign < 0 ? 1 : 0][remainder] : "Error: Input is not an integer";
+
+  return output;
 }
+
+console.log(checkEvenOrOdd(2));
+console.log(checkEvenOrOdd(3));
+console.log(checkEvenOrOdd(-4));
+console.log(checkEvenOrOdd(-4.5));
+console.log(checkEvenOrOdd("brrt"));
