c2e1a7ab5b8449901c0837576e61a89625a0b239
Ecniov <123645284+Ecniov@users.noreply.github.com>
2023-09-10 22:22:35 +0800

    vincedeato solution.js

--- src/solution.js
+++ src/solution.js
@@ -1,4 +1,23 @@
-// Function to check if a number is even or odd
-function checkEvenOrOdd(_) {
-  // Put your code here
+const readline = require('readline');
+
+// Function to check if a number is even or odd (same function as before)
+function evenOrOddExtended(input) {
+  const num = Number(input);
+  const isInteger = !isNaN(num) && Number.isInteger(num);
+  const parity = ["Even", "Odd"][(Math.floor(Math.abs(num) / 2) % 2)];
+  const sign = ["Positive", "Negative"][Math.sign(num) === -1 ? 1 : 0];
+  return isInteger ? `${sign} ${parity}` : "Error: Input is not an integer";
 }
+
+// Create a readline interface for reading user input
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// Ask the user for input and process it
+rl.question("Please enter an integer: ", (userInput) => {
+  const result = evenOrOddExtended(userInput);
+  console.log(result);
+  rl.close();
+});
