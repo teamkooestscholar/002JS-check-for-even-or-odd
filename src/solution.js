fbefc8385b68bb55fd0ab064d13afded71c2f457
SparrowNado <123578310+SparrowNado@users.noreply.github.com>
2023-09-10 15:54:34 +0800

    added html functionality  to js code

--- src/solution.js
+++ src/solution.js
@@ -1,57 +1,29 @@
 // Function to check if a number is even or odd
-// function checkEvenOrOdd(_) {
-  // Put your code here
-// }
-
-
-// * Modify the function to handle non-integer inputs gracefully. Return an error message if the input is not an integer. **[+5 extra credit points]**
-
-// * Implement the function without using conditional statements (if-else). **[+5 extra credit points]**
-
-// * Extend the function to check if the input is a positive or negative even or odd number. **[+5 extra credit points]**
-
-//npm intall prompt-sync is needed + this code below is needed to make the prompt() function work in codespaces
-const prompt=require("prompt-sync")({sigint:true});
-
-
-
 function checkEvenOrOdd(number) {
-
-  //This uses an ternary operator 
-  // it is an alternative to if else statements by making boolean arguments
-
   const result =
     !Number.isInteger(number)
-      ? "Error: Input is not an integer" // the triple equal (===) means equal 
-                                         //without type which means the type of value must be match the type of variable
+      ? "Error: Input is not an integer"
       : number === 0
       ? "Zero"
       : number > 0
       ? number % 2 === 0
-        ? "Positive Even"                //I decided to combine both the even odd and positive and negative checks
+        ? "Positive Even"
         : "Positive Odd"
       : number % 2 === 0
       ? "Negative Even"
       : "Negative Odd";
 
   return result;
- 
 }
 
+function DetermineNumber() {
+  const userInput = document.getElementById("userInput").value;
+  const number = parseInt(userInput);
+  const answer = checkEvenOrOdd(number);
 
-const usernumber = prompt("Input a number ");
+  // Display the result
+  document.getElementById('result').textContent = "The Result is: " + answer;
+}
 
-const number = parseInt(usernumber);
-
-
-const answer = checkEvenOrOdd(number)
-
-
-console.log('The Result is determined to be = ' + answer );
-
-
-
-
-
-
-
+const button = document.getElementById("checkButton");
+button.addEventListener("click", DetermineNumber);
