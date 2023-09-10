b8d8f39d781ffa086066aa475ea97cb0273c3a61
KimKris <kimberlykrismanuel@gmail.com>
2023-09-10 20:12:28 +0800

    evenorodd?

--- src/solution.js
+++ src/solution.js
@@ -1,4 +1,25 @@
 // Function to check if a number is even or odd
-function checkEvenOrOdd(_) {
-  // Put your code here
+function checkEvenOrOdd(inputNumber) {
+  const result = (inputNumber % 2 === 0) ? "Even" : "Odd";
+  return result;
 }
+
+function checkPositiveOrNegative(inputNumber) {
+  const sign = (inputNumber > 0) ? "Positive" : "Negative";
+  return sign;
+}
+
+while (true) {
+  const userNumber = parseFloat(prompt("Enter a number: "));
+
+  if (!isNaN(userNumber)) {
+    const evenOrOddResult = checkEvenOrOdd(userNumber);
+    const positiveOrNegativeResult = checkPositiveOrNegative(userNumber);
+
+    alert("The number is " + positiveOrNegativeResult + " " + evenOrOddResult);
+  } else {
+    alert("Please enter a valid number.");
+  }
+}
+ 
+
