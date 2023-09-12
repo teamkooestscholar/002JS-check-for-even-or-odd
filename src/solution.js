92ce22aefbaa4f84a5ea61dda848aa5531ea22f5
tobywan1 <shantoby00@yahoo.com>
2023-09-12 23:14:31 +0800

    Update solution.js

--- src/solution.js
+++ src/solution.js
@@ -1,4 +1,16 @@
 // Function to check if a number is even or odd
-function checkEvenOrOdd(_) {
-  // Put your code here
+function checkEvenOrOdd() {
+  const number = prompt("Enter a number:");
+  
+  if (isNaN(number)) {
+    alert("Please enter a valid number.");
+  } else {
+    if (number % 2 === 0) {
+      alert(`${number} is even.`);
+    } else {
+      alert(`${number} is odd.`);
+    }
+  }
 }
+
+checkEvenOrOdd();
